26c3be90e35042ba9e2b622caefe4bbfd930109a
Franklin <freitas.franklin@gmail.com>
2014-08-26 14:24:52 -0400

    Added ordenes base query ver 1
    Added ordenes base query version 1

--- query.php
+++ query.php
@@ -1,20 +1,10 @@
 <?php 
 
-/*
-	%% - Returns a percent sign
-	%b - Binary number
-	%c - The character according to the ASCII value
-	%d - Signed decimal number (negative, zero or positive)
-	%e - Scientific notation using a lowercase (e.g. 1.2e+2)
-	%E - Scientific notation using a uppercase (e.g. 1.2E+2)
-	%u - Unsigned decimal number (equal to or greather than zero)
-	%f - Floating-point number (local settings aware)
-	%F - Floating-point number (not local settings aware)
-	%g - shorter of %e and %f
-	%G - shorter of %E and %f
-	%o - Octal number
-	%s - String
-*/
+$baseQuery = array();
+
+$baseQuery['ordenes'] = 'SELECT * FROM cliente.finales cust, Ultracli.dordenes ordenes, ultracli.dasigna asigna 
+WHERE cust.nu_final = ordenes.nu_final AND ordenes.nu_secuencia = asigna.num_dorden';
+
 
 $queryList = array();
 
@@ -22,8 +12,7 @@ $queryList['ordenes'] = 'SELECT finales.nm_cliente, dordenes.mt_dolar FROM CLIEN
 
 $queryList['asignadas'] = 'SELECT * FROM Ultracli.dasigna';
 
-$queryList['ordenes_cliente'] = 'SELECT Finales.nm_cliente, Dordenes.mt_dolar FROM CLIENTE.Finales, Ultracli.Dordenes WHERE Finales.nu_final = Dordenes.nu_final 
-AND Finales.nu_final=%nu_final% AND fecha_valor=%fecha%';
+$queryList['ordenes_cliente'] = $baseQuery['ordenes'] . ' AND cust.nu_final=%nu_final% AND fecha_valor=%fecha%';
 
 
 
